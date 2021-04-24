f0aef5569a3ca85010751cf5d715a5c6c3d9d92b
elizeurs <13ders@gmail.com>
2021-04-24 16:37:55 -0300

    Initial Commit

---
+++ HelloMVVM/HelloMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMVVMApp.swift
+//  HelloMVVM
+//
+//  Created by Elizeu RS on 24/04/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

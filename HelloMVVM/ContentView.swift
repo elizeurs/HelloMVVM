e6733b31d3a5e08f87d4f8c993675f14d7907dd5
elizeurs <13ders@gmail.com>
2021-04-25 13:45:19 -0300

    Incrementing the Counter and Displaying on Screen

--- HelloMVVM/ContentView.swift
+++ HelloMVVM/ContentView.swift
@@ -8,17 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+  
+  @ObservedObject private var counterVM: CounterViewModel
+  
+  init() {
+    counterVM = CounterViewModel()
+  }
+  
   var body: some View {
     VStack {
       Text("PREMIUM")
         .frame(width: 200, height: 100)
         .font(.largeTitle)
       
-      Text("0")
+//      Text("0")
+      Text("\(counterVM.value)")
         .font(.title)
         .padding(.bottom)
       Button("Increment") {
-        
+        self.counterVM.increment()
       }
     }
   }
